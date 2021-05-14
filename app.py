2faef00ceec9da48fe2a3f134ddd03dd66cb5094
CristianMisake <cristiangrondon@gmail.com>
2021-05-14 13:38:36 -0500

    cambio de path

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////mnt/c/Users/NaegixMisake/proyectsPython/flaskSqlAchemy/app.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
